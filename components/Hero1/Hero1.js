38e9fbd58c4ee48c8f13b41f6f9254e2ec8d8f80
Malik Hall <malikhall247@gmail.com>
2025-05-04 18:09:13 -0600

    updated reverse css for hero section

--- components/Hero1/Hero1.js
+++ components/Hero1/Hero1.js
@@ -7,19 +7,19 @@ export default function Hero1() {
         <section className="hero-section">
             <div className="hero-carousel-container">
                 <div className="hero-carousel-wrapper">
-                    <div className="hero__wrapper">
+                    <div className="hero__wrapper reverseX">
                         <div className="hero-asset">
                             <img src="/Dwayne_Johnson_Papatui_Home_Page_Skincare_Main.png" alt="" />
                         </div>
-                        <div className="hero__text__wrapper reverseX">
+                        <div className="hero__text__wrapper">
                             <h2 className="hero__text__title">It's time to take better care</h2>
                             <div className="hero__text__btn btn">
                                 <a href="#">Shop skincare</a>
                             </div>
                         </div>
-                        <div className="hero__text__logo reverseX">
+                        <div className="hero__text__logo">
                             <h1 className="h1-logo">PAPATUI</h1>
-                            <h2 className="h2-logo text-leftX">TAKE BETTER CARE</h2>
+                            <h2 className="h2-logo">TAKE BETTER CARE</h2>
                         </div>
                     </div>
                     <div className="hero__wrapper">
